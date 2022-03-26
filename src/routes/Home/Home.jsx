9e8ed4c7b30b0f6f645f25e934d39df6f2ff4233
TempUsername2436 <christopherpokemon30@gmail.com>
2022-03-26 14:55:42 -0700

    Finished the home page

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,6 +1,15 @@
+import "./Home.scss";
+
 function Home() {
   return (
-    <div></div>
+    <section className="home">
+      <div className="container">
+        <h1>Hi, I'm<br /><span>Christopher</span></h1>
+        <p><span>For years,</span> I have studied and inscribed in this ever-expanding metaverse known to this day as the <span>Internet.</span></p>
+        <p> I constantly strive to use my <span>talent</span> in coding to transform this astonishing metaverse into something even greater.</p>
+        <p>The <span>dream land</span> I speak of <span>wasn’t</span> constructed by <span>magic,</span> but by talented individuals yearning to inscribe their dreams and fantasies into this metaverse, <span>one line of code at a time.</span></p>
+      </div>
+    </section>
   )
 }
 
