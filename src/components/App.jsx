ff82f33f74c570ec753597e91cc3f855e5daa9d9
TempUsername2436 <c.benton.145@gmail.com>
2022-03-27 09:43:06 -0700

    Finished the contact section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import "./Backgrounds/InfiniteBackground/InfiniteBackground.scss";
 import Nav from  "./Nav/Nav";
 import Home from  "../routes/Home/Home";
 import Skills from  "../routes/Skills/Skills";
+import Contact from  "../routes/Contact/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
