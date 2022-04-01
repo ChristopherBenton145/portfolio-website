2e3a01f49580261ecd69ab35720aeb55fe4ab2ac
TempUsername2436 <c.benton.145@gmail.com>
2022-04-01 13:13:41 -0700

    Added my snake-game as a new project

--- src/routes/Projects/Projects.jsx
+++ src/routes/Projects/Projects.jsx
@@ -7,6 +7,7 @@ function Projects() {
       <Project name="Restaurant Page" live="https://restaurant-page42.netlify.app" code="https://github.com/ChristopherBenton145/restaurant-page" />
       <Project name="Space Landing Page" live="https://space-landing-page42.netlify.app/" code="https://github.com/TempUsername2436/space-landing-page" />
       <Project name="Multistep Form" live="https://multistep-form8276d2.netlify.app/" code="https://github.com/ChristopherBenton145/multistep-form" />
+      <Project name="Snake Game" live="https://snake-game42.netlify.app" code="https://github.com/ChristopherBenton145/snake-game" />
     </section>
   )
 }
